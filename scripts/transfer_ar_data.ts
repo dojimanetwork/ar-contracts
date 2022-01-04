773a203ab8885d4ca366dcd25e9b35051ec5e71d
luffybhaagi <yvbgr.blockchain@gmail.com>
2022-01-04 16:36:28 +0530

    added transfer ar, ar with data scripts

--- scripts/transfer_ar_data.ts
+++ scripts/transfer_ar_data.ts
@@ -6,13 +6,17 @@ const to_addr = process.env.TARGET_ADDRESS
 const wallet = JSON.parse(fs.readFileSync("./wallet-1.json") as unknown as string);
 
 (async () => {
-
     const from = await Client.wallets.getAddress(wallet)
-    console.log("from address", from);
+    const data = process.env.TRANSFER_DATA as string
+    const byteSize = Buffer.from(data).length
+    const reward = await Client.api.get(`/price/${byteSize}/${to_addr}`)
+    console.log(reward.data);
     
     const transfer = await Client.createTransaction({
         target: to_addr,
-        quantity: Client.ar.arToWinston("1")
+        quantity: Client.ar.arToWinston("1"),
+        data,
+        reward: reward.data
     }, wallet)
 
     await Client.transactions.sign(transfer, wallet)
